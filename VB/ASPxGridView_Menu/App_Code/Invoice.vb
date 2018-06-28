0145aa3e66c1c43e9a7334e91941017b31acb772
DevExpressExampleBot <fakeEmail>
2018-06-28 17:55:52 +0300

    Source auto update [skip ci]

---
+++ VB/ASPxGridView_Menu/App_Code/Invoice.vb
@@ -0,0 +1,53 @@
+﻿Imports System
+Imports System.Collections.Generic
+
+<Serializable> _
+Public Class Invoice
+    Implements IEquatable(Of Invoice)
+
+
+    Private id_Renamed As Integer
+
+    Public Property Id() As Integer
+        Get
+            Return id_Renamed
+        End Get
+        Set(ByVal value As Integer)
+            id_Renamed = value
+        End Set
+    End Property
+
+    Private description_Renamed As String
+
+    Public Property Description() As String
+        Get
+            Return description_Renamed
+        End Get
+        Set(ByVal value As String)
+            description_Renamed = value
+        End Set
+    End Property
+
+    Private price_Renamed As Decimal
+
+    Public Property Price() As Decimal
+        Get
+            Return price_Renamed
+        End Get
+        Set(ByVal value As Decimal)
+            price_Renamed = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal id As Integer, ByVal description As String, ByVal price As Decimal)
+        Me.Id = id
+        Me.Description = description
+        Me.Price = price
+    End Sub
+    Public Sub New()
+
+    End Sub
+    Public Function Equals(ByVal other As Invoice) As Boolean Implements IEquatable(Of Invoice).Equals
+        Return Me.Id.Equals(other.Id)
+    End Function
+End Class
---
+++ VB/ASPxGridView_Menu/Default.aspx.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Linq
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub ASPxGridView1_HtmlDataCellPrepared(ByVal sender As Object, ByVal e As ASPxGridViewTableDataCellEventArgs)
+        Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+        e.Cell.Attributes("onclick") = String.Format("OnCellClick({0}, {1}, {2}, event);", grid.ClientInstanceName, e.VisibleIndex, e.DataColumn.VisibleIndex)
+    End Sub
+End Class
